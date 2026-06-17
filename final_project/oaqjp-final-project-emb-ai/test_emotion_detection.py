542c07385981a345feacf12e0ea4f555205cd879
aseemahamed <aseemnajeer@gmail.com>
2026-06-17 10:31:37 +0530

    Add contents of final project

---
+++ final_project/oaqjp-final-project-emb-ai/test_emotion_detection.py
@@ -0,0 +1,28 @@
+import unittest
+from EmotionDetection import emotion_detector
+
+class TestDetection(unittest.TestCase):
+
+    def test_joy(self):
+        response = emotion_detector("I am so happy I am doing this.")
+        self.assertEqual(response["dominant_emotion"], "joy")
+
+    def test_anger(self):
+        response = emotion_detector("I hate working long hours.")
+        self.assertEqual(response["dominant_emotion"], "anger")
+
+    def test_sadness(self):
+        response = emotion_detector("I am feeling very sad.")
+        self.assertEqual(response["dominant_emotion"], "sadness")
+
+    def test_fear(self):
+        response = emotion_detector("I am really afraid that this will happen.")
+        self.assertEqual(response["dominant_emotion"], "fear")
+
+    def test_disgust(self):
+        response = emotion_detector("I am disgusted by this behavior.")
+        self.assertEqual(response["dominant_emotion"], "disgust")
+
+
+if __name__ == "__main__":
+    unittest.main()
